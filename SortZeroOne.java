2d3d61081811d413b694195c96a6256b95a3f2f2
Tarushi <107392283+tarushi16@users.noreply.github.com>
2023-10-13 15:29:04 +0530

    Add files via upload

---
+++ SortZeroOne.java
@@ -0,0 +1,13 @@
+public class SortZeroOne {
+    public static void sortOneAndZero(int[] arr){
+        int left = 0, right = arr.length-1;
+        int temp;
+        while(left<right){
+            if(arr[left]==1){
+                if(arr[right]!=1){
+
+                }
+            }
+        }
+    }
+}
